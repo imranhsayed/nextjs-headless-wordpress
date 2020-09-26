93038cd7134f6339359f70e7683b1aadd55cead7
Imran <imranhsayed@gmail.com>
2020-09-26 13:28:05 +0530

    Setup sass, post-css and tailwind

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,6 @@
 import { ApolloProvider } from "@apollo/client";
 import client from "../src/apollo/client";
+import '../src/styles/index.scss'
 
 function MyApp({ Component, pageProps }) {
 	return (
