cc42a1058b33406f95b656ece559d1e3540ebe78
Imran <imranhsayed@gmail.com>
2020-09-27 11:39:24 +0530

    Add menu styles

--- frontend/src/components/header/navbar.js
+++ frontend/src/components/header/navbar.js
@@ -27,7 +27,7 @@ const Navbar = ({menus}) => {
 			</div>
 			<div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
 				<div className="text-sm lg:flex-grow">
-					{(menus || []).map((menu) => {
+					{(menus || []).map((menu, index) => {
 						const path = menu.node.path;
 						const href = "[...slug]";
 						return (
@@ -40,8 +40,9 @@ const Navbar = ({menus}) => {
 					})}
 				</div>
 				<div>
-					<a href="#"
-					   className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+					<Link href="/blog">
+						<a className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Blog</a>
+					</Link>
 				</div>
 			</div>
 		</nav>
