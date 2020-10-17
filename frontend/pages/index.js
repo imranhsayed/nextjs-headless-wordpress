8f9b7b39fb70752b6320183120a73cde05895824
Imran <imranhsayed@gmail.com>
2020-10-17 14:54:46 +0530

    Add staticprops

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,7 @@
 import client from "../src/apollo/client";
 import { GET_MENUS } from "../src/queries/get-menus";
 
-export default function Index() {
+export default function Home( {menus} ) {
   return (
     <div>
 	    <h3 className="text-lg leading-6 font-medium text-gray-900">
@@ -13,13 +13,16 @@ export default function Index() {
 
 export async function getStaticProps(context) {
 
-	const { data, loading, networkStatus } = await new client.query({
+	const { data, loading, networkStatus } = await client.query({
 		query: GET_MENUS
 	});
 
-	console.warn( 'data', data );
-
 	return {
-		props: {}, // will be passed to the page component as props
+		props: {
+			menus: {
+				headerMenus: data?.headerMenus?.edges,
+				footerMenus: data?.footerMenus?.edges
+			}
+		}
 	}
 }
--- frontend/src/queries/get-menus.js
+++ frontend/src/queries/get-menus.js
@@ -2,26 +2,28 @@ import { gql } from "@apollo/client";
 import MenuFragment from "./fragments/menus";
 
 export const GET_MENUS = gql`
-  headerMenus: menuItems(where: {location: HCMS_MENU_HEADER, parentId: "0"}) {
-    edges {
-      node {
-		...MenuFragment
-        childItems {
-          edges {
-            node {
-				...MenuFragment
-            }
-          }
-        }
-      }
-    }
-  }
-  footerMenus: menuItems(where: {location: HCMS_MENU_FOOTER, parentId: "0"}) {
-    edges {
-      node {
-        ...MenuFragment
-      }
-    }
-  }
+	query GET_MENUS {
+	  headerMenus: menuItems(where: {location: HCMS_MENU_HEADER, parentId: "0"}) {
+	    edges {
+	      node {
+			...MenuFragment
+	        childItems {
+	          edges {
+	            node {
+					...MenuFragment
+	            }
+	          }
+	        }
+	      }
+	    }
+	  }
+	  footerMenus: menuItems(where: {location: HCMS_MENU_FOOTER, parentId: "0"}) {
+	    edges {
+	      node {
+	        ...MenuFragment
+	      }
+	    }
+	  }
+	}
   ${MenuFragment}
 `
