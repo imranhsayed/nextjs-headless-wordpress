c98ec008484e7c898180526355c828aa06ffa3be
Imran <imranhsayed@gmail.com>
2020-11-22 15:55:02 +0530

    Update the notes

--- frontend/pages/[...slug].js
+++ frontend/pages/[...slug].js
@@ -47,6 +47,23 @@ export async function getStaticProps({ params }) {
     };
 }
 
+/**
+ * Since the page name uses catch-all routes,
+ * for example [...slug],
+ * that's why params would contain slug which is an array.
+ * For example, If we need to have dynamic route '/foo/bar'
+ * Then we would add paths: [ params: { slug: ['foo', 'bar'] } } ]
+ * Here slug will be an array is ['foo', 'bar'], then Next.js will statically generate the page at /foo/bar
+ *
+ * At build time next js will will make an api call get the data and
+ * generate a page bar.js inside .next/foo directory, so when the page is served on browser
+ * data is already present, unlike getInitialProps which gets the page at build time but makes an api
+ * call after page is served on the browser.
+ *
+ * @see https://nextjs.org/docs/basic-features/data-fetching#the-paths-key-required
+ *
+ * @returns {Promise<{paths: [], fallback: boolean}>}
+ */
 export async function getStaticPaths() {
     const { data } = await client.query({
         query: GET_PAGES_URI
