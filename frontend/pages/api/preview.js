7d88392c565e110e40e012a2fc1535e465ab520f
Imran <imranhsayed@gmail.com>
2021-02-15 11:56:13 +0530

    Update preview

--- frontend/pages/api/preview.js
+++ frontend/pages/api/preview.js
@@ -8,49 +8,12 @@ export default async function preview(req, res) {
     const { postType, postId } = req.query
 
     const authToken = getAuthToken(req)
-    console.log( 'authtokemddd', authToken );
 
     if ( isEmpty( authToken ) ) {
         res.writeHead(307, { Location: `/login/?postType=${postType}&previewPostId=${postId ?? ''}` })
+    } else {
+        const previewUrl = getPreviewRedirectUrl(postType, postId);
+        res.writeHead(307, { Location: previewUrl })
     }
-
-    const previewUrl = getPreviewRedirectUrl(postType, postId);
-
-    res.writeHead(307, { Location: previewUrl })
-
-    // Check the secret and next parameters
-    // This secret should only be known by this API route
-    // if (
-    //     !process.env.WORDPRESS_PREVIEW_SECRET ||
-    //     secret !== process.env.WORDPRESS_PREVIEW_SECRET ||
-    //    !id
-    // ) {
-    //     return res.status(401).json({ message: 'Invalid token' })
-    // }
-
-    // Fetch WordPress to check if the provided `id` or `slug` exists
-    const data = await getPreviewPage(postId)
-    // const data = await loginUser()
-    //
-    //
-    return res.status(401).json({ hey: 'hello' })
-
-    // If the post doesn't exist prevent preview mode from being enabled
-    if (!data) {
-        return res.status(401).json({ message: 'Post not found' })
-    }
-
-    // Enable Preview Mode by setting the cookies
-    res.setPreviewData({
-        post: {
-            id: data?.page?.databaseId,
-            slug: data?.page?.slug,
-            status: data?.page?.status,
-        },
-    })
-
-    // Redirect to the path from the fetched post
-    // We don't redirect to `req.query.slug` as that might lead to open redirect vulnerabilities
-    res.writeHead(307, { Location: `/${data?.page?.id ?? ''}/` })
     res.end()
 }
