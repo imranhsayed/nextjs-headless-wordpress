30d36994c86b6e335ee9bdcc475b76d2d11d9c62
Imran <imranhsayed@gmail.com>
2021-02-17 22:23:24 +0530

    Add comment

--- frontend/pages/api/login.js
+++ frontend/pages/api/login.js
@@ -5,6 +5,11 @@ export default async function login(req, res) {
     const { username, password } = req?.body ?? {};
     const data = await loginUser({username, password})
 
+    /**
+     * Note when you run 'npm run start' locally, cookies won't be set, because locally process.env.NODE_ENV = 'production'
+     * so secure will be true, but it will still be http and not https , when tested locally.
+     * So when testing locally both in dev and prod, set the value of 'secure' to be false.
+     */
     res.setHeader('Set-Cookie', cookie.serialize('auth', String(data?.login?.authToken ?? ''), {
         httpOnly: true,
         secure: 'development' !== process.env.NODE_ENV,
