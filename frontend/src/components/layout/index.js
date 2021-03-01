d5669efb13903bb14eef53625ce06d34e6e0a3b2
Imran <imranhsayed@gmail.com>
2021-03-01 23:56:21 +0530

    Update clasname

--- frontend/src/components/layout/index.js
+++ frontend/src/components/layout/index.js
@@ -32,7 +32,7 @@ const Layout = ( {data, isPost, children} ) => {
 				) : null}
 			</Head>
 			<Header header={header} headerMenus={headerMenus?.edges}/>
-			<div className="container px-5 py-24 mx-auto min-h-almost-screen">
+			<div className="md:container px-5 py-24 mx-auto min-h-almost-screen">
 				{children}
 			</div>
 			<Footer footer={footer} footerMenus={footerMenus?.edges}/>
