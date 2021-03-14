b76cb334c820af624e87866b2ea8067bcc777bc8
Imran <imranhsayed@gmail.com>
2021-03-14 20:52:31 +0530

    Update the image

--- frontend/src/components/layout/header/nav.js
+++ frontend/src/components/layout/header/nav.js
@@ -1,6 +1,7 @@
 import { isEmpty } from "lodash";
 import Link from "next/link";
 import {useState} from 'react'
+import { isCustomPageUri } from '../../../utils/slug';
 
 const Nav = ({header, headerMenus}) => {
 
@@ -39,16 +40,20 @@ const Nav = ({header, headerMenus}) => {
 			<div className={`${ isMenuVisible ? 'max-h-full' : 'h-0' } overflow-hidden w-full lg:h-full block flex-grow lg:flex lg:items-center lg:w-auto`}>
 				{ headerMenus?.length ? (
 					<div className="text-sm lg:flex-grow">
-						{ headerMenus?.map( menu => (
-							<Link key={menu?.node?.id} href={menu?.node?.path}>
-								<a
-								   className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-								   data-cy="nav-item"
-								>
-									{menu?.node?.label}
-								</a>
-							</Link>
-						) ) }
+            { headerMenus?.map( menu => {
+              if ( ! isCustomPageUri(menu?.node?.path) ) {
+                return  (
+                  <Link key={menu?.node.id} href={menu?.node?.path}>
+                    <a
+                      className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+                      data-cy="nav-item"
+                    >
+                      {menu?.node?.label}
+                    </a>
+                  </Link>
+                )
+              }
+            } ) }
 						<Link href={'/blog/'}>
 							<a
 								className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
