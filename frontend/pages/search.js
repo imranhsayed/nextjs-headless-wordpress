b326099f65e2f0233334a7564d742229d7b90784
Imran <imranhsayed@gmail.com>
2021-05-01 12:58:20 +0530

    Add searchbox

--- frontend/pages/search.js
+++ frontend/pages/search.js
@@ -2,17 +2,18 @@ import client from '../src/apollo/client';
 import { handleRedirectsAndReturnData } from '../src/utils/slug';
 import { GET_MENUS } from '../src/queries/get-menus';
 import Header from '../src/components/layout/header';
-import Link from 'next/link';
 import Footer from '../src/components/layout/footer';
 import SearchBox from '../src/components/search/search-box';
+import { useState } from 'react';
 
 export default function Search( {data} ) {
   const {header, footer, headerMenus, footerMenus} = data || {};
+  const [ searchQuery, setSearchQuery ] = useState( '' );
   return (
     <>
       <Header header={header} headerMenus={headerMenus?.edges ?? []}/>
-      <div className="h-almost-screen">
-        <SearchBox/>
+      <div className="mx-auto min-h-almost-screen">
+        <SearchBox searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
       </div>
       <Footer footer={footer} footerMenus={footerMenus?.edges ?? []}/>
     </>
