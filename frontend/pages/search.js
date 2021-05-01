c47d5e562a42ff371aa37bbd9e73737247c2be37
Imran <imranhsayed@gmail.com>
2021-05-01 12:58:42 +0530

    udpate the fields

--- frontend/pages/search.js
+++ frontend/pages/search.js
@@ -6,16 +6,16 @@ import Footer from '../src/components/layout/footer';
 import SearchBox from '../src/components/search/search-box';
 import { useState } from 'react';
 
-export default function Search( {data} ) {
-  const {header, footer, headerMenus, footerMenus} = data || {};
+export default function Search( { data } ) {
+  const { header, footer, headerMenus, footerMenus } = data || {};
   const [ searchQuery, setSearchQuery ] = useState( '' );
   return (
     <>
-      <Header header={header} headerMenus={headerMenus?.edges ?? []}/>
+      <Header header={ header } headerMenus={ headerMenus?.edges ?? [] }/>
       <div className="mx-auto min-h-almost-screen">
-        <SearchBox searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
+        <SearchBox searchQuery={ searchQuery } setSearchQuery={ setSearchQuery }/>
       </div>
-      <Footer footer={footer} footerMenus={footerMenus?.edges ?? []}/>
+      <Footer footer={ footer } footerMenus={ footerMenus?.edges ?? [] }/>
     </>
   );
 }
@@ -28,7 +28,7 @@ export async function getStaticProps( context ) {
 
   const defaultProps = {
     props: {
-      data: data || {}
+      data: data || {},
     },
     /**
      * Revalidate means that if a new request comes to server, then every 1 sec it will check
