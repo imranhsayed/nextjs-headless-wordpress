d5f62ef65f7daa8d7ca2b40aaa02e4aeeb771200
Imran <imranhsayed@gmail.com>
2021-06-12 21:44:56 +0530

    Add guteneberg styles

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -22,4 +22,25 @@ module.exports = {
 	images: {
 		domains: [ allowedImageWordPressDomain, 'via.placeholder.com' ],
 	},
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
 }
